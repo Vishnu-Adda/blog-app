f4e8942090464e670fc861412bd782cf8f84c3c1
Vishnu Adda <addavishnu@gmail.com>
2018-06-17 02:53:12 -0700

    Add files via upload
    Sanitization added in case users try to put <script> to mess up the web app. Also stylized the INDEX route.

--- RESTfulRouting/restfulblogapp/app.js
+++ RESTfulRouting/restfulblogapp/app.js
@@ -1,14 +1,16 @@
-var bodyParser = require("body-parser"), 
-methodOverride = require("method-override"),
-mongoose       = require("mongoose"),
-express        = require("express"),
-app            = express();
+var bodyParser   = require("body-parser"), 
+expressSanitizer = require("express-sanitizer"),
+methodOverride   = require("method-override"),
+mongoose         = require("mongoose"),
+express          = require("express"),
+app              = express();
   
 // APP CONFIG  
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer()); // This must go after body parser
 app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
@@ -46,6 +48,7 @@ app.get("/blogs/new", function(req, res) {
 //CREATE ROUTE
 app.post("/blogs", function(req, res) {
     // create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog) {
          if(err) {
              res.render("new");
@@ -80,6 +83,7 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 // UPDATE
 app.put("/blogs/:id", function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body); // Good use case for Middleware
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
         if(err) {
             res.redirect("/blogs");
